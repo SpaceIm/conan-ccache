ec02e1c6d3f1c25bd6583fe8bb64f3e95ae02cb0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-01 21:06:26 +0100

    ccache needs C++14 or C++17

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.33.0"
@@ -18,10 +19,10 @@ class CCacheConan(ConanFile):
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "redis_storage": [True, False],
+        "redis_storage_backend": [True, False],
     }
     default_options = {
-        "redis_storage": True,
+        "redis_storage_backend": True,
     }
 
     generators = "cmake", "cmake_find_package_multi"
@@ -31,6 +32,19 @@ class CCacheConan(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _min_cppstd(self):
+        return "17" if self.settings.compiler == "Visual Studio" else "14"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "6",
+            "clang": "6",
+            "apple-clang": "10",
+            "Visual Studio": "15.7",
+        }
+
     def export_sources(self):
         self.copy("CMakeLists.txt")
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
@@ -38,8 +52,24 @@ class CCacheConan(ConanFile):
 
     def requirements(self):
         self.requires("zstd/1.5.1")
-        if self.options.redis_storage:
+        if self.options.redis_storage_backend:
             self.requires("hiredis/1.0.2")
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._min_cppstd)
+
+        def lazy_lt_semver(v1, v2):
+            lv1 = [int(v) for v in v1.split(".")]
+            lv2 = [int(v) for v in v2.split(".")]
+            min_length = min(len(lv1), len(lv2))
+            return lv1[:min_length] < lv2[:min_length]
+
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and lazy_lt_semver(str(self.settings.compiler.version), minimum_version):
+            raise ConanInvalidConfiguration(
+                "{} requires C++{}, which your compiler does not support.".format(self.name, self._min_cppstd)
+            )
 
     def package_id(self):
         del self.info.settings.compiler
@@ -52,7 +82,7 @@ class CCacheConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.definitions["REDIS_STORAGE_BACKEND"] = self.options.redis_storage
+        self._cmake.definitions["REDIS_STORAGE_BACKEND"] = self.options.redis_storage_backend
         self._cmake.definitions["ENABLE_DOCUMENTATION"] = False
         self._cmake.definitions["ENABLE_TESTING"] = False
         self._cmake.configure()
